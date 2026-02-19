5394565f51c72e22bb81c75bbd1b3b928bb5e143
Rein Raudjärv <790553+reinra@users.noreply.github.com>
2026-02-19 23:32:11 +0200

    Added Any Color palette option

--- packages/backend/src/effects/Palette.ts
+++ packages/backend/src/effects/Palette.ts
@@ -1,5 +1,5 @@
 import { Hsl } from "@twinkly-ts/common";
-import { Color, HslColor } from "../color/Color";
+import { Color, HslColor, RgbColor } from "../color/Color";
 import { EffectParameterStorage } from "../effectParameters";
 import { OptionEffectParameter, ParameterType } from "../ParameterTypes";
 import { DEFAULT_HSL_COLOR } from "../color/Hsl";
@@ -11,6 +11,7 @@ export interface Palette {
 export enum PaletteType {
     Static = 'static',
     RandomSaturatedHsl = 'random_saturated_hsl',
+    RandomColorRgb = 'random_color_rgb',
 }
 
 export class StaticPalette implements Palette {
@@ -31,6 +32,16 @@ export class RandomSaturatedHslPalette implements Palette {
     }
 }
 
+export class RandomColorPalette implements Palette {
+    nextColor(): Color {
+        return new RgbColor({
+            red_f: Math.random(),
+            green_f: Math.random(),
+            blue_f: Math.random(),
+        });
+    }
+}
+
 export class PaletteParameters {
     public readonly parameters = new EffectParameterStorage();
     private readonly type: OptionEffectParameter = this.parameters.register({
@@ -41,7 +52,8 @@ export class PaletteParameters {
         value: PaletteType.RandomSaturatedHsl,
         options: [
             { value: PaletteType.Static, label: 'Static', description: 'Use a single static color' },
-            { value: PaletteType.RandomSaturatedHsl, label: 'Random Saturated', description: 'Generate random saturated colors in HSL space' },
+            { value: PaletteType.RandomSaturatedHsl, label: 'Saturated', description: 'Generate random saturated colors' },
+            { value: PaletteType.RandomColorRgb, label: 'Any color', description: 'Generate random colors in entire RGB space' },
         ],
     }, () => this.onUpdate());
     private readonly color = this.parameters.register({
@@ -66,6 +78,8 @@ export class PaletteParameters {
                 return new StaticPalette(new HslColor(this.color.value));
             case PaletteType.RandomSaturatedHsl:
                 return new RandomSaturatedHslPalette();
+            case PaletteType.RandomColorRgb:
+                return new RandomColorPalette();
             default:
                 throw new Error(`Unsupported palette type: ${this.type.value}`);    
         }
